50872696197a2f54f611daa7f26f9d5b4671c28a
pikin <pikin@bk.ru>
2024-04-26 21:02:50 +0500

    add right img file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.5hw"
+        applicationId = "com.example.a5hw"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
