8aeb2422d1527a4f2554b33b32eef98061b43c2c
Pikin <pikin@bk.ru>
2024-04-27 14:16:33 +0500

    binding added

--- app/src/main/java/com/example/a5hw/MainActivity.kt
+++ app/src/main/java/com/example/a5hw/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a5hw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
 
     }
 }
